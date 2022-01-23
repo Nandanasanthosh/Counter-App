c8ff36c0992c7cade7ebe108d5fa449c28cdec1c
Nandana <64355301+Nandanasanthosh@users.noreply.github.com>
2022-01-23 19:17:38 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,12 @@
+var count=78;
+document.getElementById("value").innerText=count;
+function inc(){
+    count=count+1;
+    document.getElementById("value").innerText=count;
+
+}
+function dec(){
+    count=count-1;
+    document.getElementById("value").innerText=count;
+    
+}
